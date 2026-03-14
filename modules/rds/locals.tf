f724c544fa16753081de33d6866b09515d9c8b24
ravinder <ravinder50300@gmail.com>
2026-03-14 22:21:00 +0530

    remove terraform providers and add gitignore

--- modules/rds/locals.tf
+++ modules/rds/locals.tf
@@ -1,1 +1,15 @@
 
+locals {
+  port = var.engine == "postgres" ? 5432 : 3306
+
+  family = {
+    postgres = "postgres15"
+    mysql    = "mysql8.0"
+    mariadb  = "mariadb10.6"
+  }[var.engine]
+
+  common_tags = merge(var.tags, {
+    ManagedBy = "terraform"
+    Module    = "rds"
+  })
+}
