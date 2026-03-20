a440925a83bc4039d9c7fa6ad5b19a58598692b0
ravinder <ravinder50300@gmail.com>
2026-03-20 07:34:51 +0530

    add rds module

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,14 +1,44 @@
 
-resource "aws_db_subnet_group" "rds_subnet_group" {
-  name = "${var.db_name}-subnet-group"
-  subnet_ids = var.private_subnet_ids
-
-  tags = {
-    Name = "${var.db_name}-subnet-group"
-  }
+# --- Random Password --------
+resource "random_password" "master" {
+  length = 24
+  special = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
 }
+
+# ----- Secret Manager ---------
+resource "aws_secretsmanager_secret" "rds" {
+  name =  "${var.identifier}/rds/master"
+  recovery_window_in_days = 7
+  tags = local.common_tags
+}
+
+resource "aws_secretsmanager_secret_version" "rds" {
+  secret_id = aws_secretsmanager_secret.rds.id
+  secret_string = jsonencode({
+    username = var.username
+    password = random_password.master.result
+    host = aws_db_instance.main.address
+    port = local.port
+    db_name = var.db_name
+  })
+}
+
+# ----- KMS Key for encryption at rest ------
+resource "aws_kms_key" "rds" {
+  description = "KMS key for ${var.identifier} RDS"
+  deletion_window_in_days = 10
+  enable_key_rotation = true
+  tags = local.common_tags
+}
+
+
 
 resource "aws_security_group" "rds_sg" {
   name = "${var.db_name}-subnet-group"
   vpc_id = var.vpc_id
 }
+
+
+# RDS Instance
+resource "" "" {}
--- modules/rds/versions.tf
+++ modules/rds/versions.tf
@@ -1,4 +1,13 @@
-
 terraform {
 
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.7.2"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.17.0"
+    }
+  }
 }
