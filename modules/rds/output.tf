b88a6187295c7572f0c94c4a195fe0254ae229f0
ravinder <ravinder50300@gmail.com>
2026-03-22 23:34:06 +0530

    add outputs for rds module

--- modules/rds/output.tf
+++ modules/rds/output.tf
@@ -2,3 +2,38 @@ output "db_instance_id" {
   description = "RDS Instance ID"
   value       = aws_db_instance.main.id
 }
+
+output "db_instance_address" {
+  description = "RDS endpoint address"
+  value = aws_db_instance.main.address
+}
+
+output "db_instance_port" {
+  description = "RDS port"
+  value = aws_db_instance.main.port
+}
+
+output "db_instance_arn" {
+  description = "RDS instance ARN"
+  value = aws_db_instance.main.arn
+}
+
+output "security_group_id" {
+  description = "RDS security group ID"
+  value = aws_security_group.rds_sg.id
+}
+
+output "secret_arn" {
+  description = "Secrets Manager ARN for master credentials"
+  value = aws_secretsmanager_secret.rds.arn
+}
+
+output "kms_key_arn" {
+  description = "KMS key ARN used for encryption"
+  value = aws_kms_key.rds.arn
+}
+
+output "replica_address" {
+  description = "Read replica address (if created)"
+  value = try(aws_db_instance.replica[0].address, null)
+}
